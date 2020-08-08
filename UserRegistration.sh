879f6c2fbccc16507b25f6553a39fb1eed764fc9
pradip0016 <patilpradip980@gmail.com>
2020-08-08 12:51:24 +0530

    First_name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,3 +3,15 @@
 
 echo " code for user validation using pattern "
 
+#first name
+
+echo "Enter your first name :"
+	read firstName
+	PAT="^[A-Z]{1}[a-z]{2}$"
+	if [[ $firstName =~ $PAT ]]
+	then
+		echo "Name is valid"
+	else
+		echo "Name is invalid"
+	fi
+
